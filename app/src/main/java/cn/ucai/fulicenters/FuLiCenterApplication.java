bab1c01b40f18fedd30625775123db04a3b1a95e
BaiKai <1241389931@qq.com>
2016-10-25 01:05:17 +0800

    用户登录成功以后保存到数据库中（补）

--- app/src/main/java/cn/ucai/fulicenters/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenters/FuLiCenterApplication.java
@@ -2,6 +2,8 @@ package cn.ucai.fulicenters;
 
 import android.app.Application;
 
+import cn.ucai.fulicenters.bean.User;
+
 /**
  * Created by bk124 on 2016/10/18.
  */
@@ -9,6 +11,24 @@ public class FuLiCenterApplication extends Application {
     public static FuLiCenterApplication application;
     private static FuLiCenterApplication instance;
 
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        FuLiCenterApplication.username = username;
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static void setUser(User user) {
+        FuLiCenterApplication.user = user;
+    }
+
+    private static String username;
+    private static User user;
     public void onCreate(){
         super.onCreate();
         application=this;
--- app/src/main/java/cn/ucai/fulicenters/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenters/activity/LoginActivity.java
@@ -2,18 +2,25 @@ package cn.ucai.fulicenters.activity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenters.FuLiCenterApplication;
 import cn.ucai.fulicenters.I;
 import cn.ucai.fulicenters.R;
 import cn.ucai.fulicenters.bean.Result;
 import cn.ucai.fulicenters.bean.User;
+import cn.ucai.fulicenters.dao.UserDao;
 import cn.ucai.fulicenters.net.NetDao;
 import cn.ucai.fulicenters.net.OkHttpUtils;
 import cn.ucai.fulicenters.utils.CommonUtils;
@@ -33,6 +40,11 @@ public class LoginActivity extends BaseActivity {
     String username;
     String password;
     LoginActivity mContext;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +52,9 @@ public class LoginActivity extends BaseActivity {
         ButterKnife.bind(this);
         mContext = this;
         super.onCreate(savedInstanceState);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -70,8 +85,8 @@ public class LoginActivity extends BaseActivity {
     }
 
     private void checkInput() {
-        username=musername.getText().toString().trim();
-        password=mpassword.getText().toString().trim();
+        username = musername.getText().toString().trim();
+        password = mpassword.getText().toString().trim();
         if (TextUtils.isEmpty(username)) {
             CommonUtils.showLongToast(R.string.user_name_connot_be_empty);
             musername.requestFocus();
@@ -91,18 +106,27 @@ public class LoginActivity extends BaseActivity {
         NetDao.login(mContext, username, password, new OkHttpUtils.OnCompleteListener<String>() {
             @Override
             public void onSuccess(String s) {
-                Result  result=ResultUtils.getResultFromJson(s,User.class);
-                pd.dismiss();
+                Result result = ResultUtils.getResultFromJson(s, User.class);
+//                pd.dismiss();
                 L.e(TAG, "result=" + result);
                 if (result == null) {
                     CommonUtils.showLongToast(R.string.login_fail);
                 } else {
                     if (result.isRetMsg()) {
-                       User user=(User)result.getRetData();
-                        L.e(TAG,"User="+user);
-                        MFGT.finish(mContext);
+                        User user = (User) result.getRetData();
+                        L.e(TAG, "User=" + user);
+                        UserDao dao = new UserDao(mContext);
+                        boolean isSucces = dao.saveUser(user);
+                        if (isSucces) {
+                            FuLiCenterApplication.setUser(user);
+                            MFGT.finish(mContext);
+                        } else {
+                            CommonUtils.showLongToast(R.string.user_database_error);
+                        }
+
+
                     } else {
-                        if (result.getRetCode()==I.MSG_LOGIN_UNKNOW_USER){
+                        if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER) {
                             CommonUtils.showLongToast(R.string.login_fail_unknow_user);
                         } else if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD) {
                             CommonUtils.showLongToast(R.string.login_fail_error_password);
@@ -128,5 +152,45 @@ public class LoginActivity extends BaseActivity {
             String name = data.getStringExtra(I.User.USER_NAME);
             musername.setText(name);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Login Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://cn.ucai.fulicenters.activity/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Login Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://cn.ucai.fulicenters.activity/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
