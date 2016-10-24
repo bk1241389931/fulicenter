17be6ce44e43dbcbea88753f0a5573e0f2f31b05
BaiKai <1241389931@qq.com>
2016-10-25 01:03:26 +0800

    用户登录成功以后保存到数据库中

--- app/src/main/java/cn/ucai/fulicenters/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenters/activity/SplashActivity.java
@@ -5,16 +5,24 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.ucai.fulicenters.FuLiCenterApplication;
 import cn.ucai.fulicenters.R;
+import cn.ucai.fulicenters.bean.User;
+import cn.ucai.fulicenters.dao.UserDao;
+import cn.ucai.fulicenters.utils.L;
 import cn.ucai.fulicenters.utils.MFGT;
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG=SplashActivity.class.getSimpleName();
 
     private final long sleepTime=2000;
+    SplashActivity mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mContext=this;
     }
 
     @Override
@@ -23,6 +31,12 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                User user= FuLiCenterApplication.getUser();
+                if (user==null) {
+                    UserDao dao = new UserDao(mContext);
+                    user = dao.getUser("a9527010");
+                    L.e(TAG,"database,user"+user);
+                }
                 MFGT.gotoMainActivity(SplashActivity.this);
                 finish();
             }
